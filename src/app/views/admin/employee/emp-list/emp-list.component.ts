b2cb9c6f06e86b1658ced884a4a0a1d0612af8a7
girishkolte2000 <girishkolte2000@gmail.com>
2024-06-18 14:55:33 +0530

    percentage status added

--- src/app/views/admin/employee/emp-list/emp-list.component.ts
+++ src/app/views/admin/employee/emp-list/emp-list.component.ts
@@ -8,6 +8,8 @@ import { Router } from "@angular/router";
 })
 export class EmpListComponent implements OnInit {
   @Input()
+  formattedTotalUpTime: string;
+  percentage: number;
   get color(): string {
     return this._color;
   }
@@ -17,9 +19,9 @@ export class EmpListComponent implements OnInit {
   private _color = "light";
   Employeelist: any = [];
   departmentOption: any = [];
-  role=localStorage.getItem('role');
+  role = localStorage.getItem('role');
 
-  constructor(public service: ServicesService, public router: Router) {}
+  constructor(public service: ServicesService, public router: Router) { }
 
   ngOnInit(): void {
     this.getEMployeedata();
@@ -79,4 +81,30 @@ export class EmpListComponent implements OnInit {
     // Return the complete image URL
     return `${base64Prefix}${imageType};base64,${profilePicture}`;
   }
+
+  parseTime(timeStr: string): number {
+    let timeParts = timeStr.split(' ');
+    let hours = 0, minutes = 0, seconds = 0;
+    for (let i = 0; i < timeParts.length; i += 2) {
+      if (timeParts[i + 1].includes('hour')) {
+        hours = parseInt(timeParts[i]);
+      } else if (timeParts[i + 1].includes('minute')) {
+        minutes = parseInt(timeParts[i]);
+      } else if (timeParts[i + 1].includes('second')) {
+        seconds = parseInt(timeParts[i]);
+      }
+    }
+    return hours * 3600 + minutes * 60 + seconds;
+  }
+
+  calculatePercentage(formattedTotalUpTime: string | null | undefined): number {
+    if (!formattedTotalUpTime) {
+      return 0; // Return 0 if the formattedTotalUpTime is null or undefined
+    }
+  
+    const totalSeconds = this.parseTime(formattedTotalUpTime);
+    const maxSeconds = 8 * 3600; // 8 hours in seconds
+    return (totalSeconds / maxSeconds) * 100;
+  }
+  
 }
